2967ac8a42f73617ef8487ec42a3f3b344ed87b5
SeredaOleksandr <154976460+SeredaOleksandr@users.noreply.github.com>
2024-04-08 22:32:48 +0300

    Start HW5

--- js/task-1.js
+++ js/task-1.js
@@ -1,15 +1,31 @@
-function isEnoughCapacity(products, containerSize) {
-    let sumProducts = 0;
-    const values = Object.values(products);
+// function isEnoughCapacity(products, containerSize) {
+//     let sumProducts = 0;
+//     const values = Object.values(products);
 
-    for (const value of values) {
-        sumProducts += value;
-    }
+//     for (const value of values) {
+//         sumProducts += value;
+//     }
   
-    return sumProducts <= containerSize;
+//     return sumProducts <= containerSize;
+// }
+
+// console.log(isEnoughCapacity({ apples: 2, grapes: 3, carrots: 1 }, 8)); // true
+// console.log(isEnoughCapacity({ apples: 4, grapes: 6, lime: 16 }, 12)); // false
+// console.log(isEnoughCapacity({ apples: 1, lime: 5, tomatoes: 3 }, 14)); // true
+// console.log(isEnoughCapacity({ apples: 18, potatoes: 5, oranges: 2 }, 7)); // false
+
+
+function deliverPizza(pizzaName) {
+  return `Delivering ${pizzaName} pizza.`;
 }
 
-console.log(isEnoughCapacity({ apples: 2, grapes: 3, carrots: 1 }, 8)); // true
-console.log(isEnoughCapacity({ apples: 4, grapes: 6, lime: 16 }, 12)); // false
-console.log(isEnoughCapacity({ apples: 1, lime: 5, tomatoes: 3 }, 14)); // true
-console.log(isEnoughCapacity({ apples: 18, potatoes: 5, oranges: 2 }, 7)); // false
+function makePizza(pizzaName) {
+  return `Pizza ${pizzaName} is being prepared, please wait...`;
+}
+
+function makeMessage(pizzaName, callback) {
+  return callback(pizzaName);
+}
+
+console.log(makeMessage("Royal Grand", makePizza)); // "Pizza Royal Grand is being prepared, please wait..."
+console.log(makeMessage("Ultracheese", deliverPizza)); //"Delivering Ultracheese pizza."
